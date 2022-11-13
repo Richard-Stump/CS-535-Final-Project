ef4b6c09a829015746132f15155e5e98395c38fc
Richard Stump <rnstump@purdue.edu>
2022-11-13 17:54:08 -0500

    Get debug text working on the screen

--- Multiprogram/Programs/Lab3Richard/src/Main.cpp
+++ Multiprogram/Programs/Lab3Richard/src/Main.cpp
@@ -3,40 +3,33 @@
 #  ____|   |    ____|   |        | |____|
 # |     ___|   |____ ___|    ____| |    \    PS2DEV Open Source Project.
 #-----------------------------------------------------------------------
-# Copyright 2022, ps2dev - http://www.ps2dev.org
+# Copyright 2001-2004, ps2dev - http://www.ps2dev.org
 # Licenced under Academic Free License version 2.0
 # Review ps2sdk README & LICENSE files for further details.
-#
 */
 
-#include <kernel.h>
-#include <stdint.h>
 #include <stdio.h>
-#include <ps2sdkapi.h>
-#include <dma.h>
-
-#include <packet2.h>
-
-#include <stdint.h>
-
-#include <glmModdedFunctions.hpp>
-
-const float lineData[] = {
-    0, 0,
-    200, 200
-};
-const uint32_t numLines = 2;
-
-void sendDataToVu1(void* ee_ptr, void* vu1_ptr, size_t numBytes)
-{
-}
+#include <tamtypes.h>
+#include <sifrpc.h>
+#include <debug.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[])
-{    
-    dma_channel_initialize(DMA_CHANNEL_VIF1, nullptr, 0);
-	dma_channel_fast_waits(DMA_CHANNEL_VIF1);
+{
+  SifInitRpc(0);
+  init_scr();
 
+  scr_clear();
+  scr_printf("Hello, World!\n");
 
+  // After 5 seconds, clear the screen.
+  sleep(10);
 
-    return 0;
+  // Move cursor to 20, 20
+  scr_setXY(20, 20);
+  scr_printf("Hello Again, World!\n");
+
+  sleep(10);
+
+  return 0;
 }
